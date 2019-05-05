30f3c04f724db7894cbb81ba2da93521a6241f18
BoryanaLen <44115110+BoryanaLen@users.noreply.github.com>
2019-05-05 17:43:34 +0300

    Create 01. Day of Week.cs

---
+++ 03 ARRAYS - Lesson/01. Day of Week.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int number = int.Parse(Console.ReadLine());
+
+            if(number>=1 && number <= 7)
+            {
+                Console.WriteLine(days[number-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
